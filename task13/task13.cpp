c5b6000975ebc2dcef4c7a9a4fd265f68f2c9cf6
konovashka <anton_konovalov_1@mail.ru>
2021-11-12 23:14:09 +0300

    Добавьте файлы проекта.

---
+++ task13/task13.cpp
@@ -0,0 +1,20 @@
+﻿// Частотный анализатор
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[10] = { 0 };
+    int x,ost;
+    scanf_s("%d", &x);
+    while (x>0)
+    {
+        ost = x % 10;
+        a[ost] += 1;
+        x /= 10;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d %d\n", i, a[i]);
+    }
+}
